aa97a9a114288873958157b7be037fc7d3105e69
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-11-13 21:03:41 +0100

    ej2: redone

--- src/com/Ejercicio2.java
+++ src/com/Ejercicio2.java
@@ -1,11 +1,21 @@
 package com;
 
-public class Ejercicio1 {
+/**
+ * Crea un hilo que imprima su nombre cada vez que se ejecuta (por ejemplo,
+ * "Ejecutando: Hilo-Usuario").
+ */
+public class Ejercicio2 {
+
 	public static void main(String[] args) {
-		Runnable ej1 = () -> System.out.println("Hola desde un hilo");
-		Thread hilo = new Thread(ej1);
+		Runnable ej2 = () -> System.out.println("Ejecutando: " + Thread.currentThread().getName());
+		Thread hilo = new Thread(ej2);
+		hilo.setName("Hilo-Usuario");
+
+		hilo.start();
 		
-		System.out.println("Hola desde el Main");
-		hilo.run();
+		Thread hilo2 = new Thread(ej2);
+		hilo2.setName("Otro-Usuario");
+		hilo2.start();
 	}
+
 }
