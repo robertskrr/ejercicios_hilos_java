467b6ad68f88260eec4972d7f475b187badac909
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-11-14 20:10:52 +0100

    ej2: comentado

--- src/com/Ejercicio2.java
+++ src/com/Ejercicio2.java
@@ -7,12 +7,18 @@ package com;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
+		// En el main, crea un Thread con una lambda.
+		// Dentro de la lambda, usa Thread.currentThread().getName() para mostrar el
+		// nombre.
 		Runnable ej2 = () -> System.out.println("Ejecutando: " + Thread.currentThread().getName());
 		Thread hilo = new Thread(ej2);
+
+		// Antes de start(), llama a setName("Hilo-Usuario").
 		hilo.setName("Hilo-Usuario");
 
 		hilo.start();
-		
+
+		// Repite con varios hilos con nombres distintos.
 		Thread hilo2 = new Thread(ej2);
 		hilo2.setName("Otro-Usuario");
 		hilo2.start();
